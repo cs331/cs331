97c7179f5166a73cb940d1ef5463d78351058df5
Kushal Hada (developer) <kushaldeveloper@gmail.com>
2017-05-11 09:24:29 -0400

    begin concurrency control chapter
    Signed-off-by: Kushal Hada (developer) <kushaldeveloper@gmail.com>

---
+++ book/primer/a4.sql
@@ -0,0 +1,8 @@
+insert into text values ('Smart Guy', 'Nice Book', 1999, '0123456789012');
+insert into text (author, title, isbn, year) values ('Old Scholar', 'Deep Thoughts', '1234567890120', 1900);
+insert into text (author, title, isbn, year) values ('Old Scholar', 'Great Science', '1234567890121', 1901);
+insert into text (year, author, title, isbn) values (2000, 'Nasty Professor', 'Hard Tests', '1111111111111');
+
+alter table text add (opinion varchar (10));
+
+update text set opinion = 'Excellent!' where author = 'Old Scholar';
