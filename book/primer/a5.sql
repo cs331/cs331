f8e09bf3fa8225dc2c6c6cc5454922742e57f598
Kushal Hada (developer) <kushaldeveloper@gmail.com>
2017-05-23 19:13:33 -0400

    fix spelling table in a5
    Signed-off-by: Kushal Hada (developer) <kushaldeveloper@gmail.com>

--- book/primer/a5.sql
+++ book/primer/a5.sql
@@ -1,6 +1,6 @@
 create index text1_author_idx on text 1 (author); 
 
-alter tabe text1 add (rank decimal (1,0));
+alter table text1 add (rank decimal (1,0));
 alter table text1 add constraint gt check (rank between 0 and 5);
 update text1 set rank - 5 where author = 'Old Scholar';
 update text1 set rank = 0 where author = 'Nasty Professor';
