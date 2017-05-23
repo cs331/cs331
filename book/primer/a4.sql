db486d3d096e3aecbc903ae22986affe7597c71d
Kushal Hada (developer) <kushaldeveloper@gmail.com>
2017-05-23 12:38:58 -0400

    finish a4
    update, views, insert
    
    Signed-off-by: Kushal Hada (developer) <kushaldeveloper@gmail.com>

--- book/primer/a4.sql
+++ book/primer/a4.sql
@@ -8,3 +8,15 @@ alter table text add (opinion varchar (10));
 update text set opinion = 'Excellent!' where author = 'Old Scholar';
 update text set opinion = 'Bestseller' where author = 'Smart Guy';
 update text set opinion = 'Boo!! Bad!' where lower (title) like '%tests%';
+
+delete from text where (lower (opinion) like '%bad%');
+
+drop view new_text;
+create view new_text as select author, title, year, isbn from text;
+select * from new_text;
+
+create table text1 (author varchar (20), title varchar (20), year decimal (4, 0), isbn varchar (13), primary key (isbn), constraint yt1 check (year > 1800));
+
+insert into text1 (author, title, year, isbn) select * from new_text;
+
+select * from text1;
