0b43e8698944d587240f9c633f2d35b2b03bf359
Kushal Hada (developer) <kushaldeveloper@gmail.com>
2017-05-23 19:38:43 -0400

    fix typo text should be text1 in a5
    Signed-off-by: Kushal Hada (developer) <kushaldeveloper@gmail.com>

--- book/primer/a5.sql
+++ book/primer/a5.sql
@@ -5,6 +5,6 @@ alter table text1 add constraint gt check (rank between 0 and 5);
 update text1 set rank = 5 where author = 'Old Scholar';
 update text1 set rank = 0 where author = 'Nasty Professor';
 update text1 set rank = 1 where year > 1990;
-create bitmap index text1_rank_idx on text (rank);
+create bitmap index text1_rank_idx on text1 (rank);
 
 select * from text1;
